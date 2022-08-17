a377ba0232821015ded6f43194f4fd9e5bcaa7e2
langoclinh2312 <vnbboy12345@gmail.com>
2022-08-17 09:52:09 +0700

    done

---
+++ Module_GetAttributeProduct/GetAttributeSummary/Plugin/Checkout/Model/DefaultConfigProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AHT\GetAttributeSummary\Plugin\Checkout\Model;
+
+use Magento\Checkout\Model\Session as CheckoutSession;
+
+class DefaultConfigProvider
+{
+    /**
+     * @var CheckoutSession
+     */
+    protected $checkoutSession;
+
+    /**
+     * Constructor
+     *
+     * @param CheckoutSession $checkoutSession
+     */
+    public function __construct(
+        CheckoutSession $checkoutSession
+    ) {
+        $this->checkoutSession = $checkoutSession;
+    }
+
+    public function afterGetConfig(
+        \Magento\Checkout\Model\DefaultConfigProvider $subject,
+        array $result
+    ) {
+        $items = $result['totalsData']['items'];
+        foreach ($items as $index => $item) {
+            $quoteItem = $this->checkoutSession->getQuote()->getItemById($item['item_id']);
+            $result['quoteItemData'][$index]['i_n_product'] = $quoteItem->getProduct()->getAttributeText('i_n_product');
+        }
+        return $result;
+    }
+}
